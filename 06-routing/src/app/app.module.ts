997cec34c5c2c410f4af2df196b58a56ba254efd
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-28 12:29:27 +0530

    Finish routing

--- 06-routing/src/app/app.module.ts
+++ 06-routing/src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { AdminComponent } from './admin/admin.component';
-import { GuestComponent } from './guest/guest.component';
-import { DashboardComponent } from './admin/dashboard/dashboard.component';
-import { ManageClientsComponent } from './admin/manage-clients/manage-clients.component';
-import { ManageStockComponent } from './admin/manage-stock/manage-stock.component';
-import { ViewClientsComponent } from './guest/view-clients/view-clients.component';
-import { ViewStockComponent } from './guest/view-stock/view-stock.component';
+import {AppComponent} from './app.component';
+import {MainComponent} from './main/main.component';
+import {AdminComponent} from './admin/admin.component';
+import {GuestComponent} from './guest/guest.component';
+import {DashboardComponent} from './admin/dashboard/dashboard.component';
+import {DashboardComponent as GuestDashboardComponent} from './guest/dashboard/dashboard.component';
+import {ManageClientsComponent} from './admin/manage-clients/manage-clients.component';
+import {ManageStockComponent} from './admin/manage-stock/manage-stock.component';
+import {ViewClientsComponent} from './guest/view-clients/view-clients.component';
+import {ViewStockComponent} from './guest/view-stock/view-stock.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
@@ -43,13 +44,32 @@ const routes: Routes = [
         path: 'manage-stocks',
         component: ManageStockComponent
       }
-      ]
+    ]
   },
   {
     path: 'guest',
-    component: GuestComponent
+    component: GuestComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+      },
+      {
+        path: 'dashboard',
+        component: GuestDashboardComponent
+      },
+      {
+        path: 'view-clients',
+        component: ViewClientsComponent
+      },
+      {
+        path: 'view-stock',
+        component: ViewStockComponent
+      }]
   }
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -69,4 +89,5 @@ const routes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- 06-routing/src/app/guest/guest.component.ts
+++ 06-routing/src/app/guest/guest.component.ts
@@ -4,9 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-guest',
   standalone: false,
   template: `
-    <p>
-      guest works!
-    </p>
+    <header class="border-b border-b-gray-300 px-2">
+      <menu class="flex justify-between">
+        <li routerLinkActive="border-b-sky-500!" routerLink="/guest/dashboard" class="p-2 cursor-pointer border-b border-b-transparent
+hover:border-b hover:border-b-sky-500">Dashboard</li>
+        <li routerLinkActive="border-b-sky-500!" routerLink="view-clients" class="p-2 cursor-pointer border-b border-b-transparent
+hover:border-b hover:border-b-sky-500">View Clients</li>
+        <li routerLinkActive="border-b-sky-500!" routerLink="view-stock" class="p-2 cursor-pointer border-b border-b-transparent
+hover:border-b hover:border-b-sky-500">View Stock</li>
+        <li routerLinkActive="border-b-sky-500!" routerLink="/main" class="p-2 cursor-pointer border-b border-b-transparent
+hover:border-b hover:border-b-sky-500">Log Out</li>
+      </menu>
+    </header>
+    <main class="p-3">
+      <router-outlet />
+    </main>
   `,
   styleUrl: './guest.component.scss'
 })
