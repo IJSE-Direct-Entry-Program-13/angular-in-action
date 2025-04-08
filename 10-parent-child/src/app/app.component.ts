8f8af19d03979e16a455199ba51e6f6cc3ee62f2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-08 20:18:49 +0530

    Finish Angular StateManagement with Child-Parent

--- 10-parent-child/src/app/app.component.ts
+++ 10-parent-child/src/app/app.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
+import {Observable, Subject, Subscriber} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1 class="p-2 text-center">Welcome to Parent-Child Interaction in Angular</h1>
     <div class="d-flex gap-2 p-2">
-      <input type="text" class="form-control"
+      <input #ref type="text" class="form-control"
       placeholder="Eg. Add New Task">
-      <button class="btn btn-primary">ADD</button>
+      <button (click)="addTask(ref)" class="btn btn-primary">ADD</button>
     </div>
     <div class="p-2">
-      <app-task-list/>
+      <app-task-list [texts]="taskTexts" />
     </div>
   `,
   standalone: false,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  taskTexts = new Subject<string>();
+
+  addTask(ref: HTMLInputElement) {
+    if (!ref.value.trim().length) {
+      ref.select();
+      ref.focus();
+      return;
+    }
+    this.taskTexts.next(ref.value);
+    ref.value = '';
+    ref.focus();
+  }
 }
--- 10-parent-child/src/app/task-list/task-list.component.ts
+++ 10-parent-child/src/app/task-list/task-list.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
   standalone: false,
   template: `
-    @for(task of taskList; track $index){
-<!--      <app-task (delete)="deleteTask($index)">{{task}}</app-task>-->
-      <app-task [taskText]="task" (delete)="deleteTask($index)" />
+    @for (task of taskList; track $index) {
+      <!--      <app-task (delete)="deleteTask($index)">{{task}}</app-task>-->
+      <app-task [taskText]="task" (delete)="deleteTask($index)"/>
     }
   `,
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
-  taskList: Array<string> = ['Task 1', 'Task 2'];
+export class TaskListComponent implements OnChanges {
+
+  @Input()
+  texts: Observable<string> = new Observable();
+  taskList: Array<string> = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.taskList = [];
+    this.texts.subscribe(value => {
+      this.taskList.push(value);
+    });
+  }
 
   deleteTask(index: number){
     this.taskList.splice(index, 1);
