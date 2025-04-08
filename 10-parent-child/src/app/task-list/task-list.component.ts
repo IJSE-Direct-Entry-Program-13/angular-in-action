76a902fc1f5418996b786fa1a93f9a33e40f304c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-08 19:33:22 +0530

    Add task

--- 10-parent-child/src/app/task-list/task-list.component.ts
+++ 10-parent-child/src/app/task-list/task-list.component.ts
@@ -5,11 +5,15 @@ import { Component } from '@angular/core';
   standalone: false,
   template: `
     @for(task of taskList; track $index){
-      <app-task />
+      <app-task (delete)="deleteTask($index)">{{task}}</app-task>
     }
   `,
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
   taskList: Array<String> = ['Task 1', 'Task 2'];
+
+  deleteTask(index: number){
+    this.taskList.splice(index, 1);
+  }
 }
--- 10-parent-child/src/app/task/task.component.ts
+++ 10-parent-child/src/app/task/task.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-task',
   standalone: false,
   template: `
-    <div class="p-2 d-flex justify-content-between
-     align-items-center">
-      {{text}}
-      <button class="btn btn-outline-danger btn-sm">Remove</button>
-    </div>
+<!--    <div class="p-2 d-flex justify-content-between-->
+<!--     align-items-center">-->
+      <ng-content />
+      <button (click)="deleteTask()"
+        class="btn btn-outline-danger btn-sm">Remove</button>
+<!--    </div>-->
   `,
   styleUrl: './task.component.scss'
 })
 export class TaskComponent {
-  text: string = "Dummy Task";
+  @Output("delete")
+  deleteChange = new EventEmitter<void>();
+
+  deleteTask() {
+    this.deleteChange.emit();
+  }
 }
