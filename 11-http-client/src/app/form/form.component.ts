329db6650a0fcc1c442ca0eb9e4af7b48fb0f470
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-08 22:18:00 +0530

    Implement form validation

--- 11-http-client/src/app/form/form.component.ts
+++ 11-http-client/src/app/form/form.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import {Record, RecordService} from '../record.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: false,
   template: `
-    <form class="p-2 text-center">
+    <form #frmRef class="p-2 text-center" #frm="ngForm"
+          (ngSubmit)="saveRecord(frmRef, frm)">
       <div class="mb-2">
         <label for="txt-id">ID</label>
-        <input name="id" [(ngModel)]="record.id" placeholder="Eg. C001"
+        <input required pattern="^C\\d{3}$"
+               #txtId="ngModel"
+               [class.is-invalid]="txtId.invalid && txtId.dirty"
+          name="id" [(ngModel)]="record.id" placeholder="Eg. C001"
                id="txt-id" class="form-control text-center" type="text">
       </div>
       <div class="mb-2">
         <label for="txt-name">NAME</label>
-        <input name="name" [(ngModel)]="record.name" placeholder="Eg. Kasun Sampath"
+        <input required pattern="^[A-Za-z ]+$"
+               #txtName="ngModel"
+               [class.is-invalid]="txtName.invalid && txtName.dirty"
+          name="name" [(ngModel)]="record.name" placeholder="Eg. Kasun Sampath"
                id="txt-name" class="form-control text-center" type="text">
       </div>
       <div class="d-flex gap-2 justify-content-center">
@@ -32,5 +40,18 @@ export class FormComponent {
   }
 
   constructor(public recordService: RecordService) {
+  }
+
+  saveRecord(frm: HTMLFormElement, ngForm: NgForm) {
+    if (ngForm.invalid) {
+      const elm = <HTMLInputElement>frm
+        .querySelector('.ng-invalid')!;
+      elm.focus();
+      elm.select();
+    }else{
+      // Save Record
+      ngForm.reset();
+      (<HTMLInputElement>frm.querySelector('input')!).focus();
+    }
   }
 }
